fcfb24f5ac59eb91ddf61da4918e512918a1b7c7
zzj <283680636@qq.com>
2022-05-11 16:06:10 +0800

    10

--- src/DSLVclApp.pas
+++ src/DSLVclApp.pas
@@ -14,37 +14,41 @@ uses
 type
   TMessageHandler = reference to procedure(var _Msg: TMessage);
 
-function RegisterMsgHandler(const _UniqueMsgName: string; _Handler: TMessageHandler): Cardinal;
-procedure ExecOnMainThread(_Proc: TProc);
-
-implementation
-
-type
   TVclApp = class
+  private
+    class var Instance: TVclApp;
   private
     CM_EXEC_ON_MAIN_THREAD: Cardinal;
     FMsgHandlers: TDictionary<Cardinal, TMessageHandler>;
     procedure OnExecOnMainThread(var _Msg: TMessage);
     function AppMsgFilter(var _Msg: TMessage): Boolean;
   public
+    class constructor Create;
+    class destructor Destroy;
     constructor Create;
     destructor Destroy; override;
     function RegisterMsgHandler(const _UniqueMsgName: string;
       _Handler: TMessageHandler): Cardinal;
     procedure ExecOnMainThread(_Proc: TProc);
+    class function DispatchToMainThread<T>(_Callback: TProc<T>): TProc<T>; overload; static;
+    class function DispatchToMainThread<T1, T2>(_Callback: TProc<T1, T2>): TProc<T1, T2>; overload;static;
+    class function DispatchToMainThread<T1, T2, T3>(_Callback: TProc<T1, T2, T3>): TProc<T1, T2, T3>; overload; static;
+    class function DispatchToMainThread<T1, T2, T3, T4>(_Callback: TProc<T1, T2, T3, T4>): TProc<T1, T2, T3, T4>; overload;static;
   end;
 
-var
-  G_App: TVclApp;
+function RegisterMsgHandler(const _UniqueMsgName: string; _Handler: TMessageHandler): Cardinal;
+procedure ExecOnMainThread(_Proc: TProc);
+
+implementation
 
 function RegisterMsgHandler(const _UniqueMsgName: string; _Handler: TMessageHandler): Cardinal;
 begin
-  Result := G_App.RegisterMsgHandler(_UniqueMsgName, _Handler);
+  Result := TVclApp.Instance.RegisterMsgHandler(_UniqueMsgName, _Handler);
 end;
 
 procedure ExecOnMainThread(_Proc: TProc);
 begin
-  G_App.ExecOnMainThread(_Proc);
+  TVclApp.Instance.ExecOnMainThread(_Proc);
 end;
 
 { TVclApp }
@@ -57,11 +61,54 @@ begin
   CM_EXEC_ON_MAIN_THREAD := RegisterMsgHandler('__exec_on_main_thread__', OnExecOnMainThread);
 end;
 
+class constructor TVclApp.Create;
+begin
+  Instance := TVclApp.Create;
+end;
+
 destructor TVclApp.Destroy;
 begin
   Application.UnhookMainWindow(Self.AppMsgFilter);
   FMsgHandlers.Free;
   inherited;
+end;
+
+class destructor TVclApp.Destroy;
+begin
+  FreeAndNil(Instance);
+end;
+
+class function TVclApp.DispatchToMainThread<T1, T2, T3, T4>(_Callback: TProc<T1, T2, T3, T4>): TProc<T1, T2, T3, T4>;
+begin
+  Result := procedure(_1: T1; _2: T2; _3: T3; _4: T4)
+    begin
+      TVclApp.Instance.ExecOnMainThread(procedure begin _Callback(_1, _2, _3, _4) end);
+    end;
+end;
+
+class function TVclApp.DispatchToMainThread<T1, T2, T3>(_Callback: TProc<T1, T2, T3>): TProc<T1, T2, T3>;
+begin
+  Result := procedure(_1: T1; _2: T2; _3: T3)
+    begin
+      TVclApp.Instance.ExecOnMainThread(procedure begin _Callback(_1, _2, _3) end);
+    end;
+end;
+
+class function TVclApp.DispatchToMainThread<T1, T2>(_Callback: TProc<T1, T2>): TProc<T1, T2>;
+begin
+  Result := procedure(_1: T1; _2: T2)
+    begin
+      TVclApp.Instance.ExecOnMainThread(procedure begin _Callback(_1, _2) end);
+    end;
+end;
+
+
+class function TVclApp.DispatchToMainThread<T>(_Callback: TProc<T>): TProc<T>;
+begin
+  Result := procedure(_: T)
+    begin
+      TVclApp.Instance.ExecOnMainThread(procedure begin _Callback(_) end);
+    end;
 end;
 
 procedure TVclApp.ExecOnMainThread(_Proc: TProc);
@@ -98,11 +145,5 @@ begin
   if Result then
     h(_Msg);
 end;
-
-initialization
-  G_App := TVclApp.Create;
-
-finalization
-  FreeAndNil(G_App);
 
 end.
